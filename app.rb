88f98756e51cc99c037c3d899412e1de6b3bb771
Edgar <bezhuraedgar@gmail.com>
2018-09-08 23:30:51 +0300

    Add 'Contacts' page/Removed 'About' page

--- app.rb
+++ app.rb
@@ -5,10 +5,6 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
-get '/visit' do
-  erb 'Запись на стрижку:'
-end
-
-get '/about' do
-  erb :about
+get '/contacts' do
+  erb :contacts
 end
