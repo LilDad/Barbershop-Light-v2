2e726a62bdf6292bef1a6631e0059cb398743fa4
LilDad <bezhuraedgar@gmail.com>
2018-09-16 00:40:10 +0300

    Add form on 'Contacts' page

--- app.rb
+++ app.rb
@@ -15,8 +15,17 @@ end
 
 get '/visit' do
   erb :visit
+
 end
 
 get '/test' do
   erb "404"
+end
+
+post '/visit' do
+  @first_name = params[:first_name]
+  @last_name = params[:last_name]
+  output = File.open "public/users.txt", "w"
+  output.write @first_name
+  output.close
 end
